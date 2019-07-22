2ea3f86d88c8376ad60272022d570dc70341bfc0
topmonitoring <dodoboy9620@gmail.com>
2019-07-22 20:18:06 +0300

    menu item now works with higher order components nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatPage = () => <div>HAtsPage</div>;
+
 function App() {
   return (
     <div>
-      <HomePage />>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
     className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className="background-image"
@@ -17,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
   </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
